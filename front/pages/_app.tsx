7cf62a79920690b8ad5e38095b00ac0a3da6f8c5
JoseAP <josxe89@gmail.com>
2021-12-27 20:10:14 -0600

    chakra-ui added

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
+import Layout from '../components/layout'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  )
 }
-
-export default MyApp
