eb61ef2745f4af92ebb0b35521adbe005623b7bf
joseap <josxe89@gmail.com>
2022-02-12 05:12:51 -0600

    added backend structure in rust

---
+++ back-rs/src/controllers/mod.rs
@@ -0,0 +1,3 @@
+pub mod teachers;
+
+pub use self::teachers::{echo, hello, manual_hello};
---
+++ back-rs/src/main.rs
@@ -0,0 +1,16 @@
+pub mod controllers;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+pub use self::controllers::{manual_hello, echo, hello};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
