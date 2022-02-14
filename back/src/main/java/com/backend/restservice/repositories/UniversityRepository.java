080d2537c08b3250d15ca071f32799392f1778b1
joseap <josxe89@gmail.com>
2022-02-14 02:25:27 -0600

    chosing rust as a backend language

--- back/src/main/java/com/backend/restservice/repositories/UniversityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.backend.restservice.repositories;
-import com.backend.restservice.models.University;
-import org.springframework.data.repository.CrudRepository;
-
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
-public interface UniversityRepository extends CrudRepository<University, Integer>{
-
-}
