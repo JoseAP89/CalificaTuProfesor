768ae00f228c21d388c6cd07e21f951f28ccec1d
jalvarez <jalvarez@emcor.mx>
2023-01-09 02:32:10 -0600

    Manual Search Bar v1

--- client/src/app/_components/home/home.component.ts
+++ client/src/app/_components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, delay, of } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  public searchValue: string = "";
+  public options: any[];
+  public showOptions: boolean = false;
+
+  /**
+   *
+   */
+  constructor() {
+    this.options = ["Option 1","Option 2","Option 3","Option 4","Option 5","Option 6"]
+  }
+
+  onSearch(input: string) {
+    this.showOptions = true;
+    console.log(input)
+
+  }
+
+  onSelectedOption(value: string){
+    this.searchValue = value;
+    this.showOptions = false;
+  }
+
 
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './_components/home/home.component';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
