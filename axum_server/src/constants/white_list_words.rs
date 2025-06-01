59cda92fdb8a929ab4034f212990fbcceebc58d0
jalvarez <josxe89@gmail.com>
2025-06-01 12:44:23 -0600

    white word list update

--- axum_server/src/constants/white_list_words.rs
+++ axum_server/src/constants/white_list_words.rs
@@ -3,5 +3,7 @@ use phf::{phf_set};
 pub const WHITE_LIST_WORDS: phf::Set<&'static str> = phf_set! {
     "pena", "vena", "cara", "pinchar", "inyecta", "cabeza", "apasionado", "piojo", "calabaza",
     "pollo", "venganza", "venda", "pero", "con", "el", "ella", "peso", "pasion", "aburrida",
-    "cabezear"
+    "cabezear", "pal", "vdd", "cora", "pico", "piso", "bato", "pato", "pera", "pata", "casa",
+    "lo", "y", "muy", "mucho", "poco", "la", "los", "las", "ellas", "ellos", "libro", "clase",
+    "clases"
 };
