c1c5920ec2709b8e9937ed40a80c993b9a970bf1
jalvarez <josxe89@gmail.com>
2025-06-02 13:08:36 -0600

    added a function to check for singular, plural and diminutives on vulgar words

--- axum_server/src/content_filter/filter.rs
+++ axum_server/src/content_filter/filter.rs
@@ -1,4 +1,5 @@
 use phf::Set;
+use std::collections::HashSet;
 
 use crate::content_filter::{
     normalizer::WordNormalizer,  // Fixed import path
@@ -35,16 +36,16 @@ impl ContentFilter {
 
     pub fn contains_vulgar_words(&self, text: &str) -> bool {
         let words = text.split_whitespace();
-        let white_list = WHITE_LIST_WORDS;
+        let white_list = &WHITE_LIST_WORDS;
         let black_list = &self.vulgar_words;
         for word in words {
             let normalized = WordNormalizer::normalize(word);
             // Check blacklist first
-            if black_list.contains(&normalized) {
+            if self.contains_plural_singular_spanish(black_list, &normalized) {
                 return true;
             }
             // Check whitelist 
-            if white_list.contains(&normalized) {
+            if  self.contains_plural_singular_spanish(white_list, &normalized) {
                 continue;
             }
             // if passed the previous tests, then check for more advanced patterns
@@ -56,4 +57,43 @@ impl ContentFilter {
         }
         false
     }
+
+    pub fn contains_plural_singular_spanish(&self, list: &Set<&'static str>, word: &str) -> bool {
+        // Early return if exact match exists
+        if list.contains(word) {
+            return true;
+        }
+
+        // Avoid allocation when possible by working with string slices
+        if word.len() >= 2 {
+            let last_char = word.chars().last().unwrap();
+            
+            let base = match last_char {
+                'a' | 'o' => { // it is singular
+                    if word.contains("ito") || word.contains("ita"){
+                        &word[..word.len() - 3] // it contains a diminutive
+                    } else {
+                        &word[..word.len() - 1]
+                    }
+                    
+                },
+                's' if word.len() >= 3 => { // it is plural
+                    if word.contains("itos") || word.contains("itas"){
+                        &word[..word.len() - 4] // it contains a diminutive
+                    } else {
+                        &word[..word.len() - 2]
+                    }
+                },
+                _ => &"",
+            };
+            if base.len() == 0 { return false;}
+            list.contains(&format!("{}o", base).as_str()) || 
+                list.contains(&format!("{}a", base).as_str()) ||
+                list.contains(&format!("{}os", base).as_str()) ||
+                list.contains(&format!("{}as", base).as_str())
+        } else {
+            false
+        }
+    }
+
 }
