7d74a46ee17bfa5cae568082d76c92dcd52884dc
jalvarez <josxe89@gmail.com>
2025-06-03 23:10:27 -0600

    solving compose configs for the axum-server

--- axum_server/src/main.rs
+++ axum_server/src/main.rs
@@ -1,4 +1,4 @@
-use axum::{Router};
+use axum::{http::Method, Router};
 use tower_http::cors::{CorsLayer, Any};
 use std::net::SocketAddr;
 
@@ -13,7 +13,7 @@ async fn main() {
     // Create CORS layer
     let cors = CorsLayer::new()
         .allow_origin(Any) // Allows all origins
-        .allow_methods(Any) // Allows all HTTP methods
+        .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE]) // Allows all HTTP methods
         .allow_headers(Any); // Allows all headers
     // Initialize shared state
     let shared_state = AppState {};
@@ -25,9 +25,8 @@ async fn main() {
         .with_state(shared_state);
 
     // Run the server
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("Server listening on {}", addr);
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Server listening on http://0.0.0.0:3000");
     axum::serve(listener, app).await.unwrap();
 }
