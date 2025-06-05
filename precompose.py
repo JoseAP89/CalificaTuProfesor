9d31dae92c79e289c06bc9a6d527ab2131211d20
jalvarez <josxe89@gmail.com>
2025-06-05 03:17:37 -0600

    deployment changes 4

--- precompose.py
+++ precompose.py
@@ -12,19 +12,26 @@ def run(command, cwd=None):
 
 def main():
     print("🛠️ Building Angular project...")
-
-    client_dir = os.path.join(os.getcwd(), "client")
+    root = os.getcwd()
+    client_dir = os.path.join(root, "client")
 
     run("npm install", cwd=client_dir)
     run("npm run build -- --configuration production", cwd=client_dir)
 
+    print("🛠️ Building Rust project...")
+
+    rust_dir = os.path.join(root, "axum_server")
+    run("cargo build --release", cwd=rust_dir)
+
     print("🚀 Starting Docker Compose...")
     try:
         command = "docker compose up -d --build"
-        subprocess.run(command, shell=True, check=True, cwd=None)
+        subprocess.run(command, shell=True, check=True, cwd=client_dir)
     except subprocess.CalledProcessError as e:
         print(f"\n🔧 Running alternative")
         run("docker-compose -f compose.yaml up -d --build")
+
+
     print("✅ Complete successfully...")
 
 if __name__ == "__main__":
