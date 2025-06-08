705e43791dbc1612093272aea3a9606d62f68771
jalvarez <josxe89@gmail.com>
2025-06-07 21:57:35 -0600

    loading icon will appear only if it takes longer than specified time

--- client/src/app/_interceptors/loading.interceptor.ts
+++ client/src/app/_interceptors/loading.interceptor.ts
@@ -6,20 +6,32 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable, finalize } from 'rxjs';
+import { Observable, timer, empty } from 'rxjs';
+import { mergeMap, finalize } from 'rxjs/operators';
 import { LoadingService } from '../_services/loading.service';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
   private totalRequests = 0;
+  private readonly delayMs = 500; // if loading takes less than delayMs then loading icon will not appear
 
   constructor(private loadingService: LoadingService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.totalRequests++;
-    this.loadingService.show();
 
-    return next.handle(request).pipe(
+    // Create a timer that will show loading after delay
+    const showLoader$ = timer(this.delayMs).pipe(
+      mergeMap(() => {
+        if (this.totalRequests > 0) {
+          this.loadingService.show();
+        }
+        return empty(); // Return empty observable to complete
+      })
+    );
+
+    // Execute the request and handle finalization
+    const request$ = next.handle(request).pipe(
       finalize(() => {
         this.totalRequests--;
         if (this.totalRequests === 0) {
@@ -27,5 +39,38 @@ export class LoadingInterceptor implements HttpInterceptor {
         }
       })
     );
+
+    // Race between the request and the loader timer
+    return new Observable<HttpEvent<unknown>>(subscriber => {
+      let completed = false;
+      let hasLoaderShown = false;
+
+      // Subscribe to the request
+      const requestSub = request$.subscribe({
+        next: (value) => {
+          completed = true;
+          subscriber.next(value);
+        },
+        error: (err) => subscriber.error(err),
+        complete: () => subscriber.complete()
+      });
+
+      // Subscribe to the timer
+      const timerSub = showLoader$.subscribe({
+        complete: () => {
+          if (!completed) {
+            hasLoaderShown = true;
+          }
+        }
+      });
+
+      return () => {
+        requestSub.unsubscribe();
+        timerSub.unsubscribe();
+        if (hasLoaderShown && this.totalRequests === 0) {
+          this.loadingService.hide();
+        }
+      };
+    });
   }
 }
--- client/src/app/_services/loading.service.ts
+++ client/src/app/_services/loading.service.ts
@@ -1,20 +1,27 @@
 // src/app/core/services/loading.service.ts
-import { Injectable, OnDestroy } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, distinctUntilChanged } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoadingService {
   private isLoadingSubject = new BehaviorSubject<boolean>(false);
-  isLoading$ = this.isLoadingSubject.asObservable();
-
+  
+  // Use distinctUntilChanged to prevent rapid toggling
+  isLoading$ = this.isLoadingSubject.asObservable().pipe(
+    distinctUntilChanged()
+  );
 
   show() {
-    this.isLoadingSubject.next(true);
+    if (!this.isLoadingSubject.value) {
+      this.isLoadingSubject.next(true);
+    }
   }
 
   hide() {
-    this.isLoadingSubject.next(false);
+    if (this.isLoadingSubject.value) {
+      this.isLoadingSubject.next(false);
+    }
   }
 }
